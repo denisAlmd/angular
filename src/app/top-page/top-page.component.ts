cea439ba98d823089d7ed641fdf7c7fe006b5b1c
dnis.almeida@gmail.com <dnis.almeida@gmail.com>
2023-07-26 12:47:24 -0300

    criação do projeto

---
+++ src/app/top-page/top-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-top-page',
+  templateUrl: './top-page.component.html',
+  styleUrls: ['./top-page.component.css']
+})
+export class TopPageComponent {
+
+}
